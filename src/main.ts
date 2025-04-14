529046938b50370c44f0be1aa06a044789f814bd
fazliddinofficial <fazliddinquvatboyev8@gmail.com>
2025-04-14 20:13:06 +0500

    fix: issues

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 4000, ()=> {
+    console.log(`Jarvis: Server is up and running on ${process.env.PORT || 4000} port!`)
+  });
 }
 bootstrap();
