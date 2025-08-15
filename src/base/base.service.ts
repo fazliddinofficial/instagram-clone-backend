91c5175b5bd7f155f661f8d40205eb183825c2ca
fazliddinofficial <fazliddinquvatboyev8@gmail.com>
2025-08-15 21:28:21 +0500

    setup: base service

--- src/base/base.service.ts
+++ src/base/base.service.ts
@@ -1,4 +1,4 @@
-import { Model, UpdateQuery } from 'mongoose';
+import { HydratedDocument, Model, UpdateQuery } from 'mongoose';
 
 import { DataNotFoundException, ID } from '@common';
 
@@ -8,19 +8,43 @@ export abstract class BaseService<
   T,
 > {
   constructor(private readonly model: Model<T>) {}
-  async create(dto: createDto) {
+  async create(dto: createDto): Promise<HydratedDocument<T>> {
     return await this.model.create(dto);
   }
 
-  async update(id: ID, updateDto: updateDto) {
-    return await this.model.findByIdAndUpdate(id, updateDto, { new: true });
+  async update(id: ID, updateDto: updateDto): Promise<HydratedDocument<T>> {
+    const foundData = await this.model.findByIdAndUpdate(id, updateDto, {
+      new: true,
+    });
+
+    if (!foundData) {
+      throw new DataNotFoundException();
+    }
+
+    return foundData;
   }
 
-  async delete(id: ID) {
+  async delete(id: ID): Promise<HydratedDocument<T>> {
     const foundData = await this.model.findByIdAndDelete(id);
 
     if (!foundData) {
       throw new DataNotFoundException();
     }
+
+    return foundData;
+  }
+
+  async getDocumentById(id: ID): Promise<HydratedDocument<T>> {
+    const foundData = await this.model.findById(id);
+
+    if (!foundData) {
+      throw new DataNotFoundException();
+    }
+
+    return foundData;
+  }
+
+  async getAllDocuments(): Promise<Array<HydratedDocument<T>>> {
+    return await this.model.find();
   }
 }
